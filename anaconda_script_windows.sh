d75051663d2a14b2f22e899a86c391d11a13e554
kpolimis <kivan.polimis@gmail.com>
2016-09-28 17:11:58 -0700

    added installation scripts and jupyter tutorial notebook

---
+++ anaconda_script_windows.sh
@@ -0,0 +1,8 @@
+# Anaconda 4.2.0
+# download Python 3.5
+# 64 bit version
+curl -O https://repo.continuum.io/archive/Anaconda3-4.2.0-Windows-x86_64.exe
+
+# download Python 2.7
+# 64 bit version
+#curl -O https://repo.continuum.io/archive/Anaconda2-4.2.0-Windows-x86_64.exe

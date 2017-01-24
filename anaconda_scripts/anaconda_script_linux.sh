708330621aa9007da3b8eb13d2ba6571235a4e83
kpolimis <kivan.polimis@gmail.com>
2017-01-24 11:25:01 -0800

    modified anaconda script for linux

--- anaconda_scripts/anaconda_script_linux.sh
+++ anaconda_scripts/anaconda_script_linux.sh
@@ -2,8 +2,8 @@
 # download Python 3.5
 
 curl -O https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
-bash Anaconda2-4.2.0-MacOSX-x86_64.sh 
-rm Anaconda2-4.2.0-MacOSX-x86_64.sh 
+bash Anaconda3-4.2.0-Linux-x86_64.sh
+rm Anaconda3-4.2.0-Linux-x86_64.sh
 
 
 # download Python 2.7
